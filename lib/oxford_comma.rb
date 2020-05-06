5a256d24727cbe2385dd876faa9e8eda79556628
Parker Williamson <parker.williamson@gmail.com>
2020-05-06 18:00:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1 
+    array.join
+  elsif array.length == 2 
+    array.join("and")
+  else
+    last_element = array.pop
+    array.join(",") << ", and #{last_element}}"
+  end
 end
